4c90fb43bf07f1ae78fd8010607ebb8974f2055b
prakash <unique_prakash2002@yahoo.com>
2016-12-04 16:21:20 +1100

    One more overload added when rendering view 1.Now we can specify controller and action in view when rendering like View::render('controller/action')

--- Core/View.php
+++ Core/View.php
@@ -6,77 +6,113 @@ class View
 // This is how we use overloading in php
 	public static function render()
 	{
-		/* --------------------------------------------------------------------*/
-		//only passing one argument
+		//only passing the view file
 		if(func_num_args() == 1)
 		{	
+			$count_slashes = substr_count(func_get_arg(0),'/');
+				$array = [];
+				if($count_slashes == 1){
+					// controller/action
+					$array = explode('/',func_get_arg(0));
+				}
+				else
+				{
+					// admin/controller/action
+					$array = explode('/', func_get_arg(0));
+					$last = array_pop($array);
+					$array = array(implode('/', $array), $last);
+
+				}
+// if only filepath or controller/action is defined
 			if(!is_array(func_get_arg(0)))
 			{
+				//if filepath is given [not recommended in mvc]
+				if(strpos(end($array), ".php") !== false)
+				{
+				//if it is a physical file
+				
 				$view = func_get_arg(0);
-	      		$file = "../App/Views/$view"; //relative to core directory
-	      		if(is_readable($file))
-	      		{
-	      			require $file;
-	      		}
-	      		else
-	      		{
-
-	      			throw new \Exception("$file not found");
-	      		}
+      		$file = "../App/Views/$view"; //relative to core directory
+      		if(is_readable($file))
+      		{
+      			require $file;		
       		}
       		else
       		{
-	      		$modelObject = [];
-				//this will get the model object
-	      		$modelObject = func_get_args(0)[0];
-	      		$newmodel = ["model" => $modelObject];
-	      		extract($newmodel, EXTR_SKIP);
-	      		foreach(get_declared_classes() as $key => $className)
-	      		{	
-	      			if(strpos($className, "App\Controllers") !== false)
-	      			{
-						//removing the App/controllers/ and only taking rest of it
-	      				$controller = substr($className,16);
-						//if controller contain \ like admin\users then replace \ with /
-	      				if (strpos($controller,"\\") !== false)
-	      				{
-	      					$controller = str_replace("\\", "/", $controller);
-	      				}
-						//this will make singular like users to user
-	      				$controller = rtrim($controller,"s");
-						//this will give calling method like indexAction
-	      				$callingMethod =  debug_backtrace()[1]['function'];
-						//removing Action from calling method
-	      				$method = rtrim($callingMethod, "Action");
+			//echo "$file not found";
+      			throw new \Exception("$file not found");
+      		}
+      	}
+      	else
+      	{
+      	//if is a  controller and action 
+      		//need to check if method exits as well
+      		$array = explode('/',$array[0]);
+      		if (class_exists("\App\Controllers\\$array[0]\\$array[1]"))
+      		{
+      			header('Location: /'.$array[0].'/'.$array[1]);
+      		}
+      		else
+      		{
+      			throw new \Exception("$array[1] class not found");
+      			
+      		}
+      	}
 
-						$file = "../App/Views/".$controller."/".$method.".php"; //relative to core directory
-						if(is_readable($file))
-						{
-							require $file;
-						}
-						else
-						{
-							//echo "$file not found";
-							throw new \Exception("$file not found");
-						}
+      }
+      else
+      {
+      	$modelObject = [];
+			//this will get the model object
+      	$modelObject = func_get_args(0)[0];
 
+      	extract($modelObject, EXTR_SKIP);
+      	foreach(get_declared_classes() as $key => $className)
+      	{	
+				//if $className containes App\Controllers
+      		if(strpos($className, "App\Controllers") !== false)
+      		{
+					//removing the App/controllers/ and only taking rest of it
+      			$controller = substr($className,16);
+					//if controller contain \ like admin\users then replace \ with /
+      			if (strpos($controller,"\\") !== false)
+      			{
+      				$controller = str_replace("\\", "/", $controller);
+      			}
+					//this will make singular like users to user
+      			$controller = rtrim($controller,"s");
+					//this will give calling method like indexAction
+      			$callingMethod =  debug_backtrace()[1]['function'];
+					//removing Action from calling method
+      			$method = rtrim($callingMethod, "Action");
+
+					$file = "../App/Views/".$controller."/".$method.".php"; //relative to core directory
+					if(is_readable($file))
+					{
+						require $file;
+					}
+					else
+					{
+						//echo "$file not found";
+						throw new \Exception("$file not found");
 					}
 
 				}
 
+			}
+
 		}
 
 	}
-	/* --------------------------------------------------------------------*/
-	//passing view with object
+		//passing view with object
 	else if(func_num_args() == 2)
 	{
+		$modelObject = [];
 			//this will get the view to be render
 		$view = func_get_arg(0);
 			//this will get the model object
 		$modelObject = func_get_args(1)[1];
-		$newmodel = ["model" => $modelObject];
-		extract($newmodel, EXTR_SKIP);
+		extract($modelObject, EXTR_SKIP);
 
 			//extract($modelObject, EXTR_SKIP);
 			$file = "../App/Views/$view"; //relative to core directory
@@ -89,12 +125,10 @@ class View
 			//echo "$file not found";
 				throw new \Exception("$file not found");
 			}
-	}
-	
-/* --------------------------------------------------------------------*/
-//It automatically finds right view for controller and method
-	else
-	{
+		}
+		//It automatically finds right view for controller and method
+		else
+		{
 			
 			foreach(get_declared_classes() as $key => $className)
 			{	
@@ -128,7 +162,6 @@ class View
 				}
 
 			}
-			
 		}
 
 
